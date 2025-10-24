cafc8aeff0b32719dfd78d636710dc93f7f9c574
wube0965 <wubshetabate865@gmail.com>
2025-10-25 00:26:22 +0300

    last

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.get("/api/projects", (req, res) => {
       desc: "A modern, responsive netflix website built with React by integrating the Netflix API",
       tech: ["React", "API", "Node"],
       link: "https://github.com/WubshetAbate/Netflix-Clone-2025.git",
-      demo: "https://wubshetnetfliix.netlify.app/",
+      demo: "https://netflix-clone-2025-mugs.vercel.app/",
     },
     {
       id: 2,
@@ -23,7 +23,7 @@ app.get("/api/projects", (req, res) => {
       desc: "Full-stack e-commerce application with payment integration",
       tech: ["React", "Stripe", "Node", "MySQL DB, Firebase"],
       link: "https://github.com/WubshetAbate/Amazon-frontend.git",
-      demo: "https://wubshet-amazon-fullstack-app.netlify.app/",
+      demo: "https://netflix-clone-2025-web8.vercel.app/",
     },
     {
       id: 3,
@@ -31,15 +31,15 @@ app.get("/api/projects", (req, res) => {
       desc: "A Collaborative task to built an app that signup, login, post & get question with answers",
       tech: ["React", "Node", "Express", "MySQL"],
       link: "https://github.com/WubshetAbate/evangadiForum-backend.git",
-      demo: "https://wubshet-evangadiforum.netlify.app/",
+      demo: "https://evangadi-forum-frontend-omega.vercel.app/",
     },
     {
       id: 4,
-      title: "Apple website",
-      desc: "An Apple official website rebuilt as exactly similar to it",
-      tech: ["React", "Express", "Mysql"],
-      link: "https://github.com/WubshetAbate/Apple-by-React.git",
-      demo: "https://silly-naiad-a0b745.netlify.app/",
+      title: "Weather App",
+      desc: "A Weather App that shows the weather of any city in the world",
+      tech: ["HTML", "modular CSS", "JS", "API Integration"],
+      link: "https://github.com/WubshetAbate/weather-app.git",
+      demo: "https://evangadi-forum-frontend-6cwe.vercel.app/",
     },
     {
       id: 5,
@@ -47,15 +47,7 @@ app.get("/api/projects", (req, res) => {
       desc: "A Local Calculator that performs every maths calculation",
       tech: ["HTML", "CSS", "Javascript"],
       link: "https://github.com/WubshetAbate/Calculator-project.git",
-      demo: "https://wubshet-calculator.netlify.app/",
-    },
-    {
-      id: 6,
-      title: "Weather App",
-      desc: "A Weather App that shows the weather of any city in the world",
-      tech: ["HTML", "modular CSS", "JS", "API"],
-      link: "https://github.com/WubshetAbate/weather-app.git",
-      demo: "https://wubshet-weather-app.netlify.app/",
+      demo: "https://calculator-project-g1yg.vercel.app/",
     },
   ]);
 });
